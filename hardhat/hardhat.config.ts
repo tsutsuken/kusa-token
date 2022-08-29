bc988cabb5afa2352b71aaf9a1d653ac2f924d2f
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2022-08-29 11:16:55 +0900

    Chore: Add network mumbai to hardhat.config

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+
+dotenv.config();
+
+/**
+ * Set your target network!!!
+ */
+const TARGET_NETWORK = "localhost";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  defaultNetwork: TARGET_NETWORK,
+  networks: {
+    localhost: {},
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_MUMBAI}`,
+      accounts: [`${process.env.DEPLOYER_PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
