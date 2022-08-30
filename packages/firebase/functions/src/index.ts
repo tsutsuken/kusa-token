693b6eadd87914f4c5868e968944582072db1b1b
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2022-08-30 15:33:28 +0900

    feat: Add firebase function metadata

--- packages/firebase/functions/src/index.ts
+++ packages/firebase/functions/src/index.ts
@@ -1,3 +1,12 @@
 import * as functions from "firebase-functions";
 
+export const metadata = functions.https.onRequest((request, response) => {
+  functions.logger.info("metadata", {
+    query: request.query,
+  });
+  if (request.query.id === undefined) {
+    response.status(400).send("Error: No id");
+    return;
+  }
+  response.send("metadata id: " + request.query.id);
 });
