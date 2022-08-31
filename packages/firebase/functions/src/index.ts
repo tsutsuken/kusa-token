47d358bf6d49f193185f054cf9bacf7dad36c60d
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2022-08-31 10:54:03 +0900

    Revert "feat: Return metadata of token"
    This reverts commit 11af0a82b922721126569fff88f0683a1ff284f0.

--- packages/firebase/functions/src/index.ts
+++ packages/firebase/functions/src/index.ts
@@ -4,27 +4,9 @@ export const metadata = functions.https.onRequest((request, response) => {
   functions.logger.info("metadata", {
     query: request.query,
   });
-
-  const tokenId = request.query.id;
-  if (tokenId === undefined) {
+  if (request.query.id === undefined) {
     response.status(400).send("Error: No id");
     return;
   }
-
-  let jsonString = "";
-  if (tokenId == "0") {
-    // Main token
-    jsonString = `
-        {
-            "name": "草トークン",
-            "description": "草トークンとは~",
-            "image": "https://firebasestorage.googleapis.com/v0/b/kusa-token.appspot.com/o/token_images%2Fkusasatsu_sample.png?alt=media&token=92ea5fa2-f8ea-41f7-8534-94f7ffde6b62"
-        }
-        `;
-  } else {
-    // Other token
-    jsonString = "";
-  }
-
-  response.send(jsonString);
+  response.send("metadata id: " + request.query.id);
 });
