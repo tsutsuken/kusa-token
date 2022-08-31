1cff8d45e345c1a60cdde52e657f1e65b2a4c7e1
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2022-08-31 22:35:05 +0900

    refactor: Remove comment

--- packages/hardhat/contracts/KusaToken.sol
+++ packages/hardhat/contracts/KusaToken.sol
@@ -24,7 +24,6 @@ contract KusaToken is ERC1155, Ownable, ERC1155Supply {
         _mintBatch(to, ids, amounts, data);
     }
 
-    // The following functions are overrides required by Solidity.
     function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
         internal
         override(ERC1155, ERC1155Supply)
