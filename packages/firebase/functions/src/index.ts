f9e72db28a821f9f60da84be1047361e93abe91d
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2022-08-31 10:54:11 +0900

    Revert "feat: Add firebase function metadata"
    This reverts commit 693b6eadd87914f4c5868e968944582072db1b1b.

--- packages/firebase/functions/src/index.ts
+++ packages/firebase/functions/src/index.ts
@@ -1,12 +1,3 @@
 import * as functions from "firebase-functions";
 
-export const metadata = functions.https.onRequest((request, response) => {
-  functions.logger.info("metadata", {
-    query: request.query,
-  });
-  if (request.query.id === undefined) {
-    response.status(400).send("Error: No id");
-    return;
-  }
-  response.send("metadata id: " + request.query.id);
 });
